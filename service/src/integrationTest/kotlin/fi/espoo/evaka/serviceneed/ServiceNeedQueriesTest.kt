61d5748d91f70d2586b6be47eef8c141e52fe40a
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2022-06-07 18:06:42 +0300

    Add an integration test for show_for_citizen field of service_need_option

--- service/src/integrationTest/kotlin/fi/espoo/evaka/serviceneed/ServiceNeedQueriesTest.kt
+++ service/src/integrationTest/kotlin/fi/espoo/evaka/serviceneed/ServiceNeedQueriesTest.kt
@@ -5,7 +5,9 @@
 package fi.espoo.evaka.serviceneed
 
 import fi.espoo.evaka.PureJdbiTest
+import fi.espoo.evaka.insertServiceNeedOptions
 import fi.espoo.evaka.placement.PlacementType
+import fi.espoo.evaka.shared.db.Database
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -13,5 +15,16 @@ class ServiceNeedQueriesTest : PureJdbiTest(resetDbBeforeEach = true) {
     @Test
     fun getServiceNeedOptionPublicInfos() {
         assertThat(db.read { tx -> tx.getServiceNeedOptionPublicInfos(PlacementType.values().toList()) }).isEmpty()
+    }
+
+    @Test
+    fun getOnlyShownServiceNeedOptionPublicInfos() {
+        db.transaction { tx -> tx.insertServiceNeedOptions() }
+        db.transaction { tx -> tx.updateShowForCitizen() }
+        assertThat(db.read { tx -> tx.getServiceNeedOptionPublicInfos(PlacementType.values().toList()) }).hasSize(10)
+    }
+
+    fun Database.Transaction.updateShowForCitizen() {
+        createUpdate("UPDATE service_need_option SET show_for_citizen=FALSE WHERE name_fi like '%Kokopäiväinen liittyvä%'").execute()
     }
 }
