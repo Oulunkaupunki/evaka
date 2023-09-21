181646fa330a28a0c16cc43944250d14803f73ce
Mikko Savolainen <mikko.savolainen@gofore.com>
2023-09-21 09:40:13 +0300

    feat: remove unknown absence selection from employee mobile views

--- frontend/src/employee-mobile-frontend/child-attendance/actions/MarkAbsent.tsx
+++ frontend/src/employee-mobile-frontend/child-attendance/actions/MarkAbsent.tsx
@@ -71,7 +71,6 @@ export default React.memo(function MarkAbsent({
                   absenceTypes={[
                     'OTHER_ABSENCE',
                     'SICKLEAVE',
-                    'UNKNOWN_ABSENCE',
                     'PLANNED_ABSENCE'
                   ]}
                   selectedAbsenceType={selectedAbsenceType}
