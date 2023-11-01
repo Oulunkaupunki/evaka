08e7218882d75d39a4ae4412ccaaefce0dea6b72
Vesa Honkasaari <vesa.honkasaari@gofore.com>
2023-11-01 10:04:45 +0200

    Remove the Oulu AD certificate for production

--- apigw/src/shared/certificates.ts
+++ apigw/src/shared/certificates.ts
@@ -13,8 +13,7 @@ const names = [
   'espooad-internal-staging.2022.pem',
   'idp.test.espoon-voltti.fi.pem',
   'saml-signing.idp.tunnistautuminen.suomi.fi.2022.pem',
-  'saml-signing-testi.apro.tunnistus.fi.2022.pem',
-  'ouluad-internal-prod.pem'
+  'saml-signing-testi.apro.tunnistus.fi.2022.pem'
 ] as const
 
 export type TrustedCertificates = (typeof names)[number]
